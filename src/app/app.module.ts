4018efc7e19f9f8863217c9034dc446273e44e13
Brandon Fan <brandonfan1256@gmail.com>
2018-01-03 11:44:26 -0500

    connect home component with app through router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -12,7 +15,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     environment.production
       ? ServiceWorkerModule.register('/ngsw-worker.js')
-      : []
+      : [],
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
