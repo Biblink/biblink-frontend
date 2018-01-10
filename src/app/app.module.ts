e7be14164cce02de73431e395e6289bd61362d97
Brandon Fan <brandonfan1256@gmail.com>
2018-01-09 21:59:07 -0500

    create initial components for search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,42 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
-
+// components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
 
-const appRoutes: Routes = [{ path: '', component: HomeComponent }];
+// providers
+import { SearchService } from './search.service';
+import { ResultsComponent } from './results/results.component';
+import { ResultCardComponent } from './result-card/result-card.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '/search', component: SearchComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    SearchComponent,
+    ResultsComponent,
+    ResultCardComponent
+  ],
   imports: [
     BrowserModule,
     environment.production
-    ? ServiceWorkerModule.register('/ngsw-worker.js')
-    : [],
+      ? ServiceWorkerModule.register('/ngsw-worker.js')
+      : [],
     RouterModule.forRoot(appRoutes),
     ScrollToModule.forRoot()
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
