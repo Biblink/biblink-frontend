8b8fd4e52014c7e8ab08dfd2dbff49d53de6f53f
Brandon Fan <brandonfan1256@gmail.com>
2018-01-10 22:57:24 -0500

    add animation to navbar on entrance for first time.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppComponent} from '../app.component';
+
+declare const AOS: any;
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    toHome() {
+        AppComponent.navInitialized = false;
+    }
+
+    ngOnInit() {
+        if (!AppComponent.navInitialized) {
+            AOS.init();
+            AppComponent.navInitialized = !AppComponent.navInitialized;
+        }
+    }
 
 }
