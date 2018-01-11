f5fdfc3a52f1790bdd5f406aef253bc1bb5ee374
Brandon Fan <brandonfan1256@gmail.com>
2018-01-10 22:33:28 -0500

    fix problem with navbar and add language server to dev packages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,69 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+
 
 declare const AOS: any;
 declare const $: any;
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  enhanced = false;
-  isCurrent = true;
-  activated = false;
-  menuOpacity = 0;
-  menuHeight = '0';
-  menuZ = 0;
-  constructor() {}
 
-  ngOnInit() {
-    const init = [];
-    const x = setInterval(() => {
-      init.push(AOS.init({
-        disable: 'mobile'
-      }));
-      if (init.length >= 2) {
-        clearInterval(x);
-      }
-    }, 1500);
-    setTimeout(() => {
-      this.enhanced = true;
-    }, 1000);
-    $(window).scroll(function() {
-      const scroll = $(window).scrollTop();
-      if (scroll >= 300 && scroll <= 3900) {
-        $('.dotstyle').addClass('darkDot');
-      } else {
-        $('.dotstyle').removeClass('darkDot');
-      }
-    });
-    $('.dotstyle .dots li a').hover(
-      function() {
-        $('.dotstyle ul.descriptions li#' + $(this).attr('id') + '-desc').css(
-          'opacity',
-          1
-        );
-      },
-      function() {
-        $('.dotstyle ul.descriptions li#' + $(this).attr('id') + '-desc').css(
-          'opacity',
-          0
-        );
-      }
-    );
-  }
+    enhanced = false;
+    isCurrent = true;
+    activated = false;
+    menuOpacity = 0;
+    menuHeight = '0';
 
-  toggleMobileMenu() {
-    this.activated = !this.activated;
-    this.menuOpacity = this.activated ? 1 : 0;
-    this.menuZ = this.activated ? 800 : 0;
-    this.menuHeight = this.activated ? '100%' : '0';
-    if (this.activated) {
-      $('body').css('overflow', 'hidden');
-      $('html').css('overflow', 'hidden');
-    } else {
-      $('body').css('overflow', 'visible');
-      $('html').css('overflow', 'visible');
+    constructor(private title: Title) {
     }
-  }
+
+    ngOnInit() {
+        this.title.setTitle('Biblya | Home');
+        const init = [];
+        const x = setInterval(() => {
+            init.push(AOS.init());
+            if (init.length >= 2) {
+                clearInterval(x);
+            }
+        }, 1500);
+        setTimeout(() => {
+            this.enhanced = true;
+        }, 1000);
+        $(window).scroll(function () {
+            const scroll = $(window).scrollTop();
+            if (scroll >= 300 && scroll <= 4300) {
+                $('.dotstyle').addClass('darkDot');
+            } else {
+                $('.dotstyle').removeClass('darkDot');
+            }
+        });
+        $('.dotstyle .dots li a').hover(
+            function () {
+                $('.dotstyle ul.descriptions li#' + $(this).attr('id') + '-desc').css(
+                    'opacity',
+                    1
+                );
+            },
+            function () {
+                $('.dotstyle ul.descriptions li#' + $(this).attr('id') + '-desc').css(
+                    'opacity',
+                    0
+                );
+            }
+        );
+    }
+
+    toggleMobileMenu() {
+        this.activated = !this.activated;
+        this.menuOpacity = this.activated ? 1 : 0;
+        this.menuHeight = this.activated ? '100%' : '0';
+        if (this.activated) {
+            $('body').css('overflow', 'hidden');
+            $('html').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+            $('html').css('overflow', 'visible');
+        }
+    }
 }
