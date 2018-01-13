90dd58de7264d256f46c9581598f8d707b1cb669
Brandon Fan <brandonfan1256@gmail.com>
2018-01-13 02:14:46 -0500

    update styles of search input and add typescript and angular stuff.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { RouterModule, Routes } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
+import {FormsModule} from '@angular/forms';
 // components
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SearchComponent } from './search/search.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SearchComponent} from './search/search.component';
 
 // providers
-import { SearchService } from './search.service';
-import { ResultsComponent } from './results/results.component';
-import { ResultCardComponent } from './result-card/result-card.component';
+import {SearchService} from './search.service';
+import {ResultsComponent} from './results/results.component';
+import {ResultCardComponent} from './result-card/result-card.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'search', component: SearchComponent }
+    {path: '', component: HomeComponent},
+    {path: 'search', component: SearchComponent}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    FooterComponent,
-    NavbarComponent,
-    SearchComponent,
-    ResultsComponent,
-    ResultCardComponent
-  ],
-  imports: [
-    BrowserModule,
-    environment.production
-      ? ServiceWorkerModule.register('/ngsw-worker.js')
-      : [],
-    RouterModule.forRoot(appRoutes),
-    ScrollToModule.forRoot()
-  ],
-  providers: [SearchService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        FooterComponent,
+        NavbarComponent,
+        SearchComponent,
+        ResultsComponent,
+        ResultCardComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        environment.production
+            ? ServiceWorkerModule.register('/ngsw-worker.js')
+            : [],
+        RouterModule.forRoot(appRoutes),
+        ScrollToModule.forRoot()
+    ],
+    providers: [SearchService],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
