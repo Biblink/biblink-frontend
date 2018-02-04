65a3b42566f913179e379e73bb374d63b91c55fb
Brandon Fan <brandonfan1256@gmail.com>
2018-02-04 11:42:21 -0500

    add mobile compatibility for sign in and get started components. Fix design of search on mobile. Fix problem with navbar and overflow problems by initializing in app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     static navInitialized = false;
+    onHome = false;
+
+    constructor(private _route: Router) {
+        this._route.events.subscribe((event) => {
+            if (this._route.url === '/') {
+                this.onHome = true;
+            } else {
+                this.onHome = false;
+            }
+        });
+    }
+
+    ngOnInit() {
+    }
 }
