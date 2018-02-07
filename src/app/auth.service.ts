7036ec3d81ddc93a74f00f873670743cb0d123fa
Brandon Fan <brandonfan1256@gmail.com>
2018-02-07 17:55:53 -0500

    add auth service and other things that webstorm did not add

---
+++ src/app/auth.service.ts
@@ -0,0 +1,177 @@
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import {Observable} from 'rxjs/Observable';
+import {EmailUserInterface} from './interfaces/email-user.interface';
+import {User} from './interfaces/user';
+
+@Injectable()
+export class AuthService {
+    userData: User;
+    actionCodeSettings = {
+        url: 'http://0.0.0.0:4200/sign-in'
+    };
+
+    constructor(private _afAuth: AngularFireAuth) {
+    }
+
+    userLogin(provider: 'google' | 'facebook' | 'twitter' | 'email', data: EmailUserInterface = null): Promise<any> {
+        switch (provider) {
+            case 'google': {
+                return this._afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((res) => {
+                    this.userData = new User(res['displayName'], res['email'], {});
+                    return this.userData;
+                })
+                    .catch((err) => {
+                        const errorCode = err.code;
+                        const errorMessage = err.message;
+                        return {errorCode: errorCode, errorMessage: errorMessage};
+                    });
+            }
+
+            case 'facebook': {
+                return this._afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+                    .catch((err) => {
+                        const errorCode = err.code;
+                        const errorMessage = err.message;
+                        return {errorCode: errorCode, errorMessage: errorMessage};
+                    });
+            }
+
+            case 'twitter': {
+                return this._afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider());
+            }
+
+            case 'email': {
+                if (data === null || data === undefined) {
+                    return new Promise((resolve, reject) => {
+                        reject(`Please provide proper data for email login. Data Given: ${data}`);
+                    });
+                } else {
+                    return this._afAuth.auth.signInWithEmailAndPassword(data.email, data.password)
+                        .then(() => {
+                            return `User with email: ${data.email} was logged in with email authentication.`;
+                        })
+                        .catch((err) => {
+                            const errorCode = err.code;
+                            const errorMessage = err.message;
+                            if (errorCode === 'auth/wrong-password') {
+                                return {errorCode: errorCode, errorMessage: 'wrong password'};
+                            } else if (errorCode === 'auth/user-not-found') {
+                                return 'user not found';
+                            } else {
+                                return errorMessage;
+                            }
+                        });
+                }
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    userSignup(provider: 'google' | 'facebook' | 'twitter' | 'email', data: EmailUserInterface = null): Promise<any> {
+        switch (provider) {
+            case 'google': {
+                return this._afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+                    .then((res) => {
+                        const userRes = res['user'];
+                        return this.userData = new User(userRes['displayName'], userRes['email'], {});
+                    }).catch((err) => {
+                        const errorCode = err.code;
+                        const errorMessage = err.message;
+                        return {errorCode: errorCode, errorMessage: errorMessage};
+                    });
+            }
+
+            case 'facebook': {
+                return this._afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then((res) => {
+                    const userRes = res['user'];
+                    this.userData = new User(userRes['displayName'], userRes['email'], {});
+                    return this.userData;
+                }).catch((err) => {
+                    const errorCode = err.code;
+                    const errorMessage = err.message;
+                    return {errorCode: errorCode, errorMessage: errorMessage};
+                });
+            }
+
+            case 'twitter': {
+                return this._afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider()).then((res) => {
+                    console.log(res);
+                    const userRes = res['user'];
+                    this.userData = new User(userRes['displayName'], userRes['email'], {});
+                    return this.userData;
+                });
+            }
+
+            case 'email': {
+                if (data === null || data === undefined) {
+                    return new Promise((resolve, reject) => {
+                        reject(`Please provide proper data for email creation. Data Given: ${data}`);
+                    });
+                } else {
+                    return this._afAuth.auth.createUserWithEmailAndPassword(data.email, data.password)
+                        .then(() => {
+                            return this.sendVerificationEmail();
+                        })
+                        .catch((err) => {
+                            const errorCode = err.code;
+                            const errorMessage = err.message;
+                            if (errorCode === 'auth/weak-password') {
+                                return 'weak password';
+                            } else {
+                                return {errorCode: errorCode, errorMessage: errorMessage};
+                            }
+                        });
+                }
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    logout(): Promise<any> {
+        return this._afAuth.auth.signOut();
+    }
+
+    sendPasswordReset(email: string): Promise<any> {
+        return this._afAuth.auth.sendPasswordResetEmail(email, this.actionCodeSettings)
+            .then(() => {
+                return 'success';
+            })
+            .catch((error) => {
+                return {errorCode: error.code, errorMessage: error.message};
+            });
+    }
+
+
+    get authState(): Observable<firebase.User> {
+        return this._afAuth.authState;
+    }
+
+    get emailVerified(): Promise<boolean> {
+        if (this._afAuth.auth.currentUser.emailVerified) {
+            return new Promise((resolve) => {
+                resolve(true);
+            });
+        }
+        return this._afAuth.auth.currentUser.reload().then((res) => {
+            return this._afAuth.auth.currentUser.emailVerified;
+        });
+    }
+
+    get email() {
+        return this.userData.email;
+    }
+
+    sendVerificationEmail() {
+        return this._afAuth.auth.currentUser.sendEmailVerification(this.actionCodeSettings).then(function () {
+            return `Email verification was sent.`;
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+}
---
+++ src/app/verify-email/verify-email.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
+
+@Component({
+    selector: 'app-verify-email',
+    templateUrl: './verify-email.component.html',
+    styleUrls: ['./verify-email.component.css']
+})
+export class VerifyEmailComponent implements OnInit {
+    isVerified = null;
+
+    constructor(private _auth: AuthService, private router: Router, private toastr: ToastrService) {
+    }
+
+    ngOnInit() {
+    }
+
+    checkVerification() {
+        this._auth.emailVerified.then((res) => {
+            this.isVerified = res;
+            if (this.isVerified) {
+                setTimeout(() => {
+                    this.router.navigateByUrl('/'); // TODO: change this to dashboard later.
+                }, 2000);
+            }
+        });
+    }
+
+    resendVerificationEmail() {
+        if (!this._auth.emailVerified) {
+            this._auth.sendVerificationEmail().then(() => {
+                this.toastr.show(`Successfully sent a verification email to ${this._auth.email}`, 'Verified Email Sent');
+            });
+        }
+    }
+
+}
