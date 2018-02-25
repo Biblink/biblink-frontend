d2a0b46927ae4979b15d322934bc0a64d0229a58
Brandon Fan <brandonfan1256@gmail.com>
2018-02-25 14:39:58 -0500

    add code to update profile with bio and short description

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserDataService } from '../user-data.service';
 import { AuthService } from '../auth.service';
 import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,15 +14,14 @@ import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from
 export class DashboardComponent implements OnInit {
   data = null;
   tab = 'studies';
-  firstName = '';
   imageUrl = '';
 
-  lastName = '';
+  user: User = new User('', '', '', { profileImage: '', bio: '', shortDescription: '' });
   name = '';
 
   emailInUse = false;
   differentCredential = false;
-  profileForm: FormGroup;
+  securityForm: FormGroup;
   email_regex = new RegExp('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"' +
     '(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")' +
     '@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?' +
@@ -46,34 +46,27 @@ export class DashboardComponent implements OnInit {
     this._data.userData.subscribe((res) => {
       this.data = res;
       if (res !== null) {
-        this.name = res[ 'name' ];
-        this.profileForm.patchValue({ firstName: res[ 'firstName' ], lastName: res[ 'lastName' ], email: res[ 'email' ] });
-        this.imageUrl = res[ 'imageUrl' ];
+        this.user = new User(res[ 'firstName' ], res[ 'lastName' ], res[ 'email' ], res[ 'data' ]);
+        this.name = this.user.firstName + ' ' + this.user.lastName;
       }
     });
   }
 
-  updateUser(object) {
-    return this._data.updateProfile(object);
+  updateUser(user: User) {
+    return this._data.updateProfile(user);
   }
-  updateName() {
-    this.updateUser({ firstName: this.firstName, lastName: this.lastName, name: `${ this.firstName } ${ this.lastName }` });
-  }
-
   switchTab(tabName: string) {
     this.tab = tabName;
   }
 
   updateProfile() {
-    this.updateUser({ firstName: this.firstNameForm.value, lastName: this.lastNameForm.value, name: this.nameForm }).then(() => {
+    this.updateUser(this.user).then(() => {
       this.toastr.show('Successfully Updated Your Profile', 'Successful Update');
     });
   }
 
   createForm(): void {
-    this.profileForm = this.fb.group({
-      firstName: [ '' ],
-      lastName: [ '' ],
+    this.securityForm = this.fb.group({
       email: [ '', [ Validators.pattern(this.email_regex) ] ],
       password: [ '' ],
       confirmPassword: [ '' ],
@@ -83,28 +76,4 @@ export class DashboardComponent implements OnInit {
       });
   }
 
-  get firstNameForm() {
-    return this.profileForm.get('firstName');
-  }
-
-  get lastNameForm() {
-    return this.profileForm.get('lastName');
-  }
-
-  get email() {
-    return this.profileForm.get('email');
-  }
-
-  get nameForm() {
-    return this.profileForm.get('firstName').value + ' ' + this.profileForm.get('lastName').value;
-  }
-
-
-  get password() {
-    return this.profileForm.get('password');
-  }
-
-  get confirmPassword() {
-    return this.profileForm.get('confirmPassword');
-  }
 }
