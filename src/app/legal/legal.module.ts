9a6bfa3e6b4d8f5c21be88d41a660e2855ee7487
Brandon Fan <brandonfan1256@gmail.com>
2018-06-14 23:47:49 -0400

    add routing configuration for legal/support/organization

--- src/app/legal/legal.module.ts
+++ src/app/legal/legal.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalRoutingModule } from './legal-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    LegalRoutingModule
   ],
-  declarations: [TermsOfUseComponent, PrivacyPolicyComponent]
+  declarations: [ TermsOfUseComponent, PrivacyPolicyComponent ]
 })
 export class LegalModule { }
--- src/app/support/support.module.ts
+++ src/app/support/support.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FAQComponent } from './faq/faq.component';
 import { SupportCenterComponent } from './support-center/support-center.component';
+import { SupportRoutingModule } from './support-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SupportRoutingModule
   ],
-  declarations: [FAQComponent, SupportCenterComponent]
+  declarations: [ FAQComponent, SupportCenterComponent ]
 })
 export class SupportModule { }
