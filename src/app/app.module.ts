ed41215317758786f865dfe13bafd444b0e0d0a9
Brandon Fan <brandonfan1256@gmail.com>
2018-06-15 00:43:17 -0400

    remove old files that are not in shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
-import { SafeHtmlPipe } from './pipes/safe-html.pipe';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
+
+
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { Angulartics2Module } from 'angulartics2';
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 import { ShareModule } from '@ngx-share/core';
 import { ClipboardModule } from 'ngx-clipboard';
 import { ToastrModule } from 'ngx-toastr';
@@ -17,17 +15,22 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RecaptchaModule } from 'ng-recaptcha';
 import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// analytics
-import { Angulartics2Module } from 'angulartics2';
-import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
+// custom modules
+import { SharedModule } from './shared/shared.module';
+import { OrganizationModule } from './organization/organization.module';
+import { AppRoutingModule } from './/app-routing.module';
+import { LegalModule } from './legal/legal.module';
+import { SupportModule } from './support/support.module';
+import { HomeModule } from './home/home.module';
+import { AboutModule } from './about/about.module';
+
 
 
 // components
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
 import { ResultsComponent } from './results/results.component';
 import { ResultCardComponent } from './result-card/result-card.component';
@@ -41,23 +44,16 @@ import { PasswordResetComponent } from './password-reset/password-reset.componen
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
 import { UserDataService } from './user-data.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { DropZoneDirective } from './drop-zone.directive';
-import { FileUploadComponent } from './file-upload/file-upload.component';
 import { StudyDataService } from './study-data.service';
 import { StudyComponent } from './study/study.component';
 import { StudyNavComponent } from './study-nav/study-nav.component';
-import { PostCardComponent } from './post-card/post-card.component';
-import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { NotFinishedComponent } from './not-finished/not-finished.component';
-import { AppRoutingModule } from './/app-routing.module';
+
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent,
-        FooterComponent,
-        NavbarComponent,
         SearchComponent,
         ResultsComponent,
         ResultCardComponent,
@@ -66,36 +62,34 @@ import { AppRoutingModule } from './/app-routing.module';
         PasswordResetComponent,
         VerifyEmailComponent,
         DashboardComponent,
-        DropZoneDirective,
-        FileUploadComponent,
         StudyComponent,
         StudyNavComponent,
-        PostCardComponent,
-        LoadingSpinnerComponent,
-        SafeHtmlPipe,
         NotFinishedComponent
     ],
     imports: [
-        Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ]),
-        BrowserModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        ReactiveFormsModule,
+        SharedModule,
         AngularFireModule.initializeApp(environment.firebase, 'biblya'),
         AngularFirestoreModule.enablePersistence(),
         AngularFireAuthModule,
         AngularFireStorageModule,
         ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
         RecaptchaModule.forRoot(),
+        Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ]),
         RecaptchaFormsModule,
         ScrollToModule.forRoot(),
         HttpClientModule,
         HttpClientJsonpModule,
         ClipboardModule,
         ShareModule.forRoot(),
+        BrowserAnimationsModule,
         ToastrModule.forRoot({
             positionClass: 'toast-bottom-left'
         }),
+        HomeModule,
+        AboutModule,
+        SupportModule,
+        LegalModule,
+        OrganizationModule,
         AppRoutingModule
     ],
     providers: [ SearchService, AuthService, UserDataService, StudyDataService ],
