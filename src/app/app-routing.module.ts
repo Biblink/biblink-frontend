5d821250894c37b8831a4d7306fcaf26adc10d3d
Brandon Fan <brandonfan1256@gmail.com>
2018-06-14 23:19:09 -0400

    convert routes into separate module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { StudyComponent } from './study/study.component';
+import { NotFinishedComponent } from './not-finished/not-finished.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: 'get-started', component: GetStartedComponent },
+  { path: 'password-reset', component: PasswordResetComponent },
+  { path: 'dashboard/home', component: DashboardComponent },
+  { path: 'dashboard', pathMatch: 'full', redirectTo: '/dashboard/home' },
+  { path: 'dashboard/studies/study/:id', component: StudyComponent },
+  { path: 'not-finished', component: NotFinishedComponent }
+];
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+
+})
+export class AppRoutingModule { }
