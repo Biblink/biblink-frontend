dd34d201ce2dd0a7e9beb7b0e3e2ccdea25263c5
Brandon Fan <brandonfan1256@gmail.com>
2018-06-23 15:19:54 -0400

    fix(tooltip): fix problem with tooltip not being fully displayed

--- src/app/study/components/post-card/post-card.component.ts
+++ src/app/study/components/post-card/post-card.component.ts
@@ -34,7 +34,7 @@ export class PostCardComponent implements OnInit {
   replyText = '';
   subReplyText = '';
   replyID = '';
-  activateZ = -10;
+  activateZ = 0;
   contributorImages = [];
   constructor(
     private afs: AngularFirestore,
