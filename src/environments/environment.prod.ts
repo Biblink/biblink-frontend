d56f6c38d400288e0ce9279faefbef8d14f1bd6b
Brandon Fan <brandonfan1256@gmail.com>
2018-06-29 23:47:59 -0400

    fix(circle-ci): add environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  es_url: 'https://api.ogs.bhsswimanddive.com',
+  firebase: {
+    apiKey: 'AIzaSyAruFcRRzxkzNydXszNkZiHJE6fbqOHpf0',
+    authDomain: 'biblya-ed2ec.firebaseapp.com',
+    databaseURL: 'https://biblya-ed2ec.firebaseio.com',
+    projectId: 'biblya-ed2ec',
+    storageBucket: 'biblya-ed2ec.appspot.com',
+    messagingSenderId: '4312138799'
+  }
+}; // change this if you are doing a demo
