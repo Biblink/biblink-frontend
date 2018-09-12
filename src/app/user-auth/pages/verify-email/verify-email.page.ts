7e107ac8b109d653ebdf15b6901f186e82213eb6
Brandon Fan <brandonfan1256@gmail.com>
2018-09-12 11:53:39 -0400

    feat(welcome-email): add welcome email on email verification

--- src/app/user-auth/pages/verify-email/verify-email.page.ts
+++ src/app/user-auth/pages/verify-email/verify-email.page.ts
@@ -2,6 +2,7 @@ import { AuthService } from './../../../core/services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Verify email component to display verify email page
@@ -23,7 +24,7 @@ export class VerifyEmailComponent implements OnInit {
      * @param {Router} router Router dependency to access to router for navigation
      * @param {ToastrService} toastr Toastr service dependency to display notifications (toasts)
      */
-    constructor(private _auth: AuthService, private router: Router, private toastr: ToastrService) {
+    constructor(private _auth: AuthService, private router: Router, private toastr: ToastrService, private http: HttpClient) {
     }
     /**
      * Initializes component
@@ -42,6 +43,17 @@ export class VerifyEmailComponent implements OnInit {
                 }, 2000);
             }
         });
+    }
+
+    sendWelcomeEmail() {
+        const welcomeEndpoint = 'https://us-central1-biblya-ed2ec.cloudfunctions.net/sendWelcomeEmail';
+        const data = {
+            email: this._auth.email,
+            name: this._auth.userData.firstName
+        };
+        this.http.post(welcomeEndpoint, data).subscribe()
+
+
     }
     /**
      * Function to resend email verification
