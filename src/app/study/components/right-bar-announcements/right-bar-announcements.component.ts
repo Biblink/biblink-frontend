e3663e93d1af452657128cd947d21ab8a3116cc3
Brandon Fan <brandonfan1256@gmail.com>
2018-11-24 18:43:45 -0500

    feat(right-bar): update right bar modularization

--- src/app/study/components/right-bar-announcements/right-bar-announcements.component.ts
+++ src/app/study/components/right-bar-announcements/right-bar-announcements.component.ts
@@ -1,30 +1,28 @@
-import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { Component, Output, Input, EventEmitter, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { StudyDataService } from '../../services/study-data.service';
 import { UserDataService } from '../../../core/services/user-data/user-data.service';
 import { unescapeIdentifier } from '../../../../../node_modules/@angular/compiler';
 
-@Component ({
-    selector: 'right-bar-announcement',
-    templateUrl: './right-bar-announcements.component.html',
-    styleUrls: ['./right-bar-announcements.component.css']
+@Component({
+  selector: 'right-bar-announcement',
+  templateUrl: './right-bar-announcements.component.html',
+  styleUrls: ['./right-bar-announcements.component.css']
 })
-
-export class RightBarAnnouncments {
-
- /**
+export class RightBarAnnouncments implements OnInit, OnDestroy {
+  /**
    * List of key announcements
    */
   keyAnnouncements = [];
-   /**
+  /**
    * @ignore
    */
   keyAnnouncementSubscription: Subscription = new Subscription();
-   /**
+  /**
    * Group ID
    */
-  groupID = 'default';
+  @Input() groupID = 'default';
   /**
    * Behavior Subject to handle loading state
    */
@@ -37,15 +35,15 @@ export class RightBarAnnouncments {
    * @ignore
    */
   postSubscription: Subscription = new Subscription();
-    /**
+  /**
    * List of posts to be checked
    */
   private _posts = new BehaviorSubject([]);
-   /**
+  /**
    * Post type to filter by
    */
   type = 'all';
-    /**
+  /**
    * Number of members to display in sidebar
    */
   numberOfMembers = 3;
@@ -53,7 +51,7 @@ export class RightBarAnnouncments {
    * List of members
    */
   members = [];
-   /**
+  /**
    * @ignore
    */
   membersSubscription: Subscription = new Subscription();
@@ -66,27 +64,23 @@ export class RightBarAnnouncments {
 
   @Output() promote = new EventEmitter<string[]>();
 
-
-
   expandActions() {
-      this.action.emit(true);
+    this.action.emit(true);
   }
 
   setPostType() {
-      this.announcement.emit('announcement')
+    this.announcement.emit('announcement');
   }
 
-  switchTab(tab : string) {
-      this.tab.emit(tab)
+  switchTab(tab: string) {
+    this.tab.emit(tab);
   }
 
-  verifyPromote(name: string, uid : string){
-      this.promote.emit([name, uid])
+  verifyPromote(name: string, uid: string) {
+    this.promote.emit([name, uid]);
   }
 
-
-
-   /**
+  /**
    * Destroys component and unsubscribes from all subscriptions
    */
   ngOnDestroy() {
@@ -97,21 +91,26 @@ export class RightBarAnnouncments {
 
   constructor(
     private _study: StudyDataService,
-    private _user: UserDataService,
-  ) { }
+    private _user: UserDataService
+  ) {}
+  ngOnInit() {
+    console.log('here');
+    this.getKeyAnnouncements();
+    this.getMembers();
+  }
 
   /**
    * Checks if htmlText property is present
    */
   private _checkHtmlText(val: any) {
-    val[ 'htmlText' ] =
-      val[ 'htmlText' ] === undefined || val[ 'htmlText' ] === ''
-        ? val[ 'text' ]
-        : val[ 'htmlText' ];
+    val['htmlText'] =
+      val['htmlText'] === undefined || val['htmlText'] === ''
+        ? val['text']
+        : val['htmlText'];
     return val;
   }
 
-   /**
+  /**
    * Get posts by a specific type
    */
   private _getFeedByType(type: string): void {
@@ -127,8 +126,7 @@ export class RightBarAnnouncments {
         this._posts.next(res);
       });
   }
-  
-   /**
+  /**
    * Get study announcements
    */
   getAnnouncements() {
@@ -146,20 +144,25 @@ export class RightBarAnnouncments {
       .getKeyAnnouncements(this.groupID)
       .pipe(
         map(res => {
+          console.log('here');
           this.keyAnnouncements = [];
           res.map(val => {
             val = this._checkHtmlText(val);
             const contained = this.keyAnnouncements.filter(
-              value => value[ 'id' ] === val[ 'id' ]
+              value => value['id'] === val['id']
             );
             this._user
-              .getDataFromID(val[ 'creatorID' ])
+              .getDataFromID(val['creatorID'])
               .take(1)
               .subscribe(response => {
-                val[ 'image' ] = response[ 'data' ][ 'profileImage' ];
+                val['image'] = response['data']['profileImage'];
+                if (val['image'] === null) {
+                  val['image'] =
+                    '/assets/images/feature-images/default-photo.png';
+                }
                 if (contained.length === 1) {
                   this.keyAnnouncements[
-                    this.keyAnnouncements.indexOf(contained[ 0 ])
+                    this.keyAnnouncements.indexOf(contained[0])
                   ] = val;
                 } else {
                   this.keyAnnouncements.push(val);
@@ -182,31 +185,38 @@ export class RightBarAnnouncments {
         members.forEach(member => {
           let firstTime = false;
           let oldImage = { name: '', uid: '', image: '', role: '' };
-          this._user.getDataFromID(member[ 'uid' ]).subscribe(res => {
+          this._user.getDataFromID(member['uid']).subscribe(res => {
             if (firstTime) {
-              this.members[ this.members.indexOf(oldImage) ] = {
-                name: res[ 'name' ],
-                image: res[ 'data' ][ 'profileImage' ],
-                role: member[ 'role' ],
-                uid: member[ 'uid' ]
+              this.members[this.members.indexOf(oldImage)] = {
+                name: res['name'],
+                image: res['data']['profileImage'],
+                role: member['role'],
+                uid: member['uid']
               };
             } else {
               this.members.push({
-                name: res[ 'name' ],
-                uid: member[ 'uid' ],
-                image: res[ 'data' ][ 'profileImage' ],
-                role: member[ 'role' ]
+                name: res['name'],
+                uid: member['uid'],
+                image: res['data']['profileImage'],
+                role: member['role']
               });
             }
             firstTime = true;
             oldImage = {
-              name: res[ 'name' ],
-              uid: member[ 'uid' ],
-              image: res[ 'data' ][ 'profileImage' ],
-              role: member[ 'role' ]
+              name: res['name'],
+              uid: member['uid'],
+              image: res['data']['profileImage'],
+              role: member['role']
             };
           });
         });
       });
+  }
+  /**
+   * Capitalizes a string
+   * @param {string} str String to capitalize
+   */
+  capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.substr(1);
   }
 }
